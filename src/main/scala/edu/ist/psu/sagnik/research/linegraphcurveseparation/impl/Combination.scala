e4a8c3b6c4b755da88ecfd8a920e80f55fa819b6
sagnik <sagnik1987@gmail.com>
2016-03-12 15:53:29 -0500

    added combintation with custom rejection logic

--- src/main/scala/edu/ist/psu/sagnik/research/linegraphcurveseparation/impl/Combination.scala
+++ src/main/scala/edu/ist/psu/sagnik/research/linegraphcurveseparation/impl/Combination.scala
@@ -7,45 +7,35 @@ import reflect.runtime.universe._
  * Created by szr163 on 3/11/16.
  */
 
-//adopted from http://aperiodic.net/phil/scala/s-99/p26.scala
 class Combination[A] {
 
-  type A=Int
-
-  def flatMapSublists[A,B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
-    ls match {
-      case Nil => Nil
-      case sublist@(_ :: tail) =>  f(sublist) ::: flatMapSublists(tail)(f)
-    }
-
-
-  //def myRejectFunc[A](xs:List[A]):Boolean=false
-
-  //def testReject[A<:Int](x:A):Boolean=x>5
-  /*
-  def myRejectFunc[A](xs:List[A]):Boolean= A.type match{
-    case Int => xs.forall(y=>xs.exists(x=>(y.asInstanceOf[Int]-x.asInstanceOf[Int])==1))
-    case _ => false
-  }
- */
-
-  def myRejectFunc[A](xs:List[A]):Boolean=
-    if(xs.forall(a=>a.isInstanceOf[Int])) xs.forall(y=>xs.exists(x=>(y.asInstanceOf[Int]-x.asInstanceOf[Int])==1))
-    else false
-
-
-
-
-  def combination[A](n: Int, ls: List[A]): List[List[A]] =
-    if (n == 0) List(Nil)
-    else flatMapSublists(ls) { sl =>
-      combination(n - 1, sl.tail) map {sl.head :: _}
-    }
+  def combination[A](r: Int, ls: List[A], rejectFunc:(A,List[A])=>Boolean): List[List[A]] =
+    if (r == 1) ls.map(x => List(x))
+    else
+      combination(
+        r - 1,
+        ls,
+        rejectFunc
+      ).flatMap(x => for (l <- ls if !rejectFunc(l,x)) yield x :+ l ).map(x=>x.toSet).distinct.map(y=>y.toList)
 }
 
 object TestCombination{
+
+  //reject function will say if an element l should be put into a list ls. If it returns true, we should NOT put l in ls.
+  def myrejectFunc[A](l:A, ls:List[A]):Boolean= ls.contains(l) ||
+    //custom logic here
+    {
+      if ( l.isInstanceOf[Int] && ls.forall(a=>a.isInstanceOf[Int])){
+        val ils=ls.map(x=>x.asInstanceOf[Int])
+        val il=l.asInstanceOf[Int]
+        (il::ils).contains(2)
+        //!ils.exists(x=>((il-x)==1)||((x-il)==1))
+      }
+      else false
+    }
+
   def main(args:Array[String])={
-    val l=List(1,2,3,4)
-    println(new Combination[Int].combination[Int](2,l))
+    val l=List(1,2,3,4,5)
+    println(new Combination[Int].combination[Int](2,l,myrejectFunc))
   }
 }
