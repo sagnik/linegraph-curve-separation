c4f4320237f4fb3ab16363f7a750c669c1ea2053
sagnik <sagnik1987@gmail.com>
2016-03-12 19:47:23 -0500

    use the tail recursive combination

--- src/main/scala/edu/ist/psu/sagnik/research/linegraphcurveseparation/impl/Combination.scala
+++ src/main/scala/edu/ist/psu/sagnik/research/linegraphcurveseparation/impl/Combination.scala
@@ -1,8 +1,5 @@
 package edu.ist.psu.sagnik.research.linegraphcurveseparation.impl
 
-//import scala.reflect.api.TypeTags.TypeTag
-import reflect.runtime.universe._
-
 /**
  * Created by szr163 on 3/11/16.
  */
@@ -17,9 +14,28 @@ class Combination[A] {
         ls,
         rejectFunc
       ).flatMap(x => for (l <- ls if !(x.contains(l) || rejectFunc(l,x))) yield x :+ l ).map(x=>x.toSet).distinct.map(y=>y.toList)
+
+  def combinationTL[A](r: Int, startingR: Int, ls: List[A], rejectFunc:(A,List[A])=>Boolean, combs:List[List[A]]): List[List[A]] =
+    if (r == startingR) combs
+    else
+      combinationTL(
+        r,
+        startingR+1,
+        ls,
+        rejectFunc,
+       combs.flatMap(x => for (l <- ls if !(x.contains(l) || rejectFunc(l,x))) yield x :+ l ).map(x=>x.toSet).distinct.map(y=>y.toList)
+      )
 }
 
 object TestCombination{
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
+  }
 
   //reject function will say if an element l should be put into a list ls. If it returns true, we should NOT put l in ls.
   def myrejectFunc[A](l:A, ls:List[A]):Boolean= //custom logic here, ex, a 3 combintation of elements with 2 not present.
@@ -31,9 +47,19 @@ object TestCombination{
       }
       else false
 
+  def myrejectFunc2[A](l:A, ls:List[A]):Boolean= false
 
-  def main(args:Array[String])={
-    val l=List(1,2,3,4,5)
-    println(new Combination[Int].combination[Int](2,l,myrejectFunc))
+  def main(args:Array[String]):Unit={
+    val l=(1 to 1000).toList
+    val r=2
+    //println(l.combinations(2).filterNot(x=>x.contains(2)).toList)
+    //println(new Combination[Int].combinationTL[Int](2,1,l,myrejectFunc,l.map(x=>List(x))))
+
+    println("computation started")
+    assert(
+    time{l.combinations(r).filterNot(x=>x.contains(2)).toList}
+      ==
+    time{new Combination[Int].combinationTL[Int](r,1,l,myrejectFunc,l.map(x=>List(x)))}
+    )
   }
 }
